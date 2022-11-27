994d5a1fced1f45f26335ad9a59d8c6b2e28cd0d
mohamed bahgat <mhmd.bahgat0@gmail.com>
2022-11-27 23:56:32 +0200

    Add Product image services and add brand property to product model

--- ITI.Ecommerce.Models/Product/Product.cs
+++ ITI.Ecommerce.Models/Product/Product.cs
@@ -11,6 +11,7 @@ namespace ITI.Ecommerce.Models
         public int ID { get; set; }
         public string NameAR { get; set; }
         public string NameEN { get; set; }
+        public string Brand { get; set; }
         public string Description { get; set; }
         public int CategoryID { get; set; }
         public int Quantity { get; set; }
