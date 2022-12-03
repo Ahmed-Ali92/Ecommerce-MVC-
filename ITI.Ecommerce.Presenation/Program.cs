8fc3af9d2ce64d137fa764a889beb452747be393
mohamed bahgat <mhmd.bahgat0@gmail.com>
2022-12-03 15:15:15 +0200

    commit

--- ITI.Ecommerce.Presenation/Program.cs
+++ ITI.Ecommerce.Presenation/Program.cs
@@ -1,55 +1,56 @@
-﻿
-
-using ITI.Ecommerce.Models;
+﻿using ITI.Ecommerce.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
 
-public class Program
+namespace ITI.Ecommerce.Presenation
 {
-    public static int Main()
+    public class Program
     {
-        var builder = WebApplication.CreateBuilder();
-        builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
-        builder.Services.AddControllersWithViews();
-        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        public static int Main()
         {
-            options.UseSqlServer("Data Source=.;initial catalog = ITI.EcommerceDB; integrated security = true;");
-        });
+            var builder = WebApplication.CreateBuilder();
+            builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer("Data Source=.;initial catalog = ITI.EcommerceDB; integrated security = true;");
+            });
 
-        builder.Services.AddIdentity<Customer, IdentityRole>
-           ().AddEntityFrameworkStores<ApplicationDbContext>();
+            builder.Services.AddIdentity<Customer, IdentityRole>
+               ().AddEntityFrameworkStores<ApplicationDbContext>();
 
-        builder.Services.AddControllersWithViews();
-
-
-        builder.Services.Configure<IdentityOptions>(options =>
-        {
-            options.User.AllowedUserNameCharacters= string.Empty;
-            options.Password.RequireDigit = false;
-            options.Password.RequireLowercase = false;
-            options.Password.RequireUppercase = false;
-            options.Password.RequireNonAlphanumeric = false;
-        });
+            builder.Services.AddControllersWithViews();
 
 
+            builder.Services.Configure<IdentityOptions>(options =>
+            {
+                options.User.AllowedUserNameCharacters = string.Empty;
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+            });
 
 
-        var app = builder.Build();
-        app.UseStaticFiles(new StaticFileOptions()
-        {
-            RequestPath = "/Content",
-            FileProvider = new PhysicalFileProvider
-            (Path.Combine(Directory.GetCurrentDirectory(),
-            "Content"))
-        });
 
-        app.UseAuthentication();
-        app.UseAuthorization();
-        app.MapControllerRoute("main", "{controller=User}/{action=SignIn}");
 
-        app.Run();
+            var app = builder.Build();
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                RequestPath = "/Content",
+                FileProvider = new PhysicalFileProvider
+                (Path.Combine(Directory.GetCurrentDirectory(),
+                "Content"))
+            });
 
-        return 0;
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.MapControllerRoute("main", "{controller=User}/{action=SignIn}");
+
+            app.Run();
+
+            return 0;
+        }
     }
 }
